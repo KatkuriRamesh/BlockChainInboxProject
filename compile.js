ec52ab208f6677cbb8bf05c4740c66c9f49602aa
Ramesh <katkuri.ramesh@impressico.com>
2018-10-09 11:10:45 +0530

    Compilejs Changes

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
-const inboxpath=path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source =fs.readFileSync(inboxpath,'utf8');
+const lotterypath=path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source =fs.readFileSync(lotterypath,'utf8');
 
-module.exports=solc.compile(source,1).contracts[':Inbox'];
+module.exports=solc.compile(source,1).contracts[':Lottery'];
